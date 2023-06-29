cd138d74a527d2439b008c43fa9b41101880a9a5
Kyce <135944962+Kyce-Sheppard@users.noreply.github.com>
2023-06-29 13:40:43 -0500

    Add files via upload
    practice using basic dictionaries

---
+++ dictionary_pract_people_info.py
@@ -0,0 +1,8 @@
+friend_0 = {'first': 'kyce', 'last': 'sheppard', 'location': 'oklahoma'}
+friend_1 = {'first': 'steve', 'last': 'shoesmith', 'location': 'london'}
+friend_2 = {'first': 'alex', 'last': 'christmas', 'location': 'france'}
+
+people = [friend_0, friend_1, friend_2]
+
+for friend in people:
+	print(friend)
